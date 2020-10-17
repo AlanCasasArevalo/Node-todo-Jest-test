e792f463340293c6fe76a0e11e675f41e0dbaf11
Alan Casas Arevalo <alan.casas.arevalo@everis.com>
2020-10-17 10:17:34 +0200

    [REFACTOR][TEST] * Todo controller test was refactored to create before each test a clear copy from request, response and next, to do not repeat code

--- test/unit/todo.controler.test.js
+++ test/unit/todo.controler.test.js
@@ -4,6 +4,13 @@ const httpsMocks = require('node-mocks-http')
 const newTodo = require('../mock-data/new-todo.json.json')
 
 TodoModel.create = jest.fn()
+let req, res, next
+
+beforeEach(() => {
+    req = httpsMocks.createRequest()
+    res = httpsMocks.createResponse()
+    next = null
+})
 
 describe('TodoController', () => {
     it('It todoController should be defined', () => {
@@ -14,19 +21,8 @@ describe('TodoController', () => {
         expect(typeof todoController.createTodo).toBe('function')
     })
 
-    it('should call TodoModel.create', function () {
-        todoController.createTodo()
-        expect(TodoModel.create).toBeCalled()
-    });
-
     it('Todo controller should received req, res and next', function () {
-        let req, res, next
-        req = httpsMocks.createRequest()
-        res = httpsMocks.createResponse()
-
-        next = null
         req.body = newTodo
-
         todoController.createTodo(req, res, next)
         expect(TodoModel.create).toBeCalledWith(newTodo)
     });
