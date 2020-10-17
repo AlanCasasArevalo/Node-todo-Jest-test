6a06498cdff499498cb2e6c147030a837585f0f9
Alan Casas Arevalo <alan.casas.arevalo@everis.com>
2020-10-17 12:44:21 +0200

    [TEST][INTEGRATION TODO CONTROLLER] * Integration test to check if controller throws an error if some parameters are missing was implemented

--- test/integration/todo.controller.int.test.js
+++ test/integration/todo.controller.int.test.js
@@ -4,7 +4,7 @@ const endpointUrl = '/todos/'
 const newTodo = require('../mock-data/new-todo.json.json')
 
 describe(endpointUrl, () => {
-    test(`POST ${endpointUrl}`, async () => {
+    it(`POST ${endpointUrl}`, async () => {
         const response = await request(app)
             .post(endpointUrl)
             .send(newTodo)
@@ -12,5 +12,14 @@ describe(endpointUrl, () => {
         expect(response.statusCode).toBe(201)
         expect(response.body.title).toBe(newTodo.title)
         expect(response.body.done).toBe(newTodo.done)
+    })
+
+    it(`Should return an error 500 on malformed data with POST ${endpointUrl}`, async () => {
+        const response = await request(app)
+            .post(endpointUrl)
+            .send({
+                title: "Make first unit test",
+            })
+        expect(response.statusCode).toBe(500)
     })
 })
