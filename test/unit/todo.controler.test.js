afd1617e896565c44004e653a002029405646178
Alan Casas Arevalo <alan.casas.arevalo@everis.com>
2020-10-17 13:15:46 +0200

    [TEST][GET TODOS] * First Get all todos test was added to ensure that functions exist

--- test/unit/todo.controler.test.js
+++ test/unit/todo.controler.test.js
@@ -12,47 +12,58 @@ beforeEach(() => {
     next = jest.fn()
 })
 
-describe('TodoController', () => {
+describe('', () => {
+    describe('TodoController GET Todo', () => {
+        it('Should have a getTodos function', () => {
+            expect(typeof todoController.getTodos).toBe('function')
 
-    beforeEach(() => {
-        req.body = newTodo
+
+        })
     })
 
-    it('It todoController should be defined', () => {
-        expect(todoController).toBeDefined()
+    describe('TodoController POST Todo', () => {
+
+        beforeEach(() => {
+            req.body = newTodo
+        })
+
+        it('It todoController should be defined', () => {
+            expect(todoController).toBeDefined()
+        })
+
+        it('It should have a createTodo Function', () => {
+            expect(typeof todoController.createTodo).toBe('function')
+        })
+
+        it('Todo controller should received req, res and next', function () {
+            todoController.createTodo(req, res, next)
+            expect(TodoModel.create).toBeCalledWith(newTodo)
+        });
+
+        it('Should return 201 response code when everything it is ok', async function () {
+            await todoController.createTodo(req, res, next)
+            expect(res.statusCode).toBe(201)
+            expect(res._isEndCalled()).toBeTruthy()
+        });
+
+        it('Should return JSON body in response when everything it is ok', async function () {
+            TodoModel.create.mockReturnValue(newTodo)
+            await todoController.createTodo(req, res, next)
+            expect(res._getJSONData()).toStrictEqual(newTodo)
+        });
+
+        it('Should handle errors', async function () {
+            const errorMessage = {
+                message: 'Some properties required was not sent'
+            }
+            const rejectedPromise = Promise.reject(errorMessage)
+            TodoModel.create.mockReturnValue(rejectedPromise)
+            await todoController.createTodo(req, res, next)
+            expect(next).toBeCalledWith(errorMessage)
+        });
     })
-
-    it('It should have a createTodo Function', () => {
-        expect(typeof todoController.createTodo).toBe('function')
-    })
-
-    it('Todo controller should received req, res and next', function () {
-        todoController.createTodo(req, res, next)
-        expect(TodoModel.create).toBeCalledWith(newTodo)
-    });
-
-    it('Should return 201 response code when everything it is ok', async function () {
-        await todoController.createTodo(req, res, next)
-        expect(res.statusCode).toBe(201)
-        expect(res._isEndCalled()).toBeTruthy()
-    });
-
-    it('Should return JSON body in response when everything it is ok', async function () {
-        TodoModel.create.mockReturnValue(newTodo)
-        await todoController.createTodo(req, res, next)
-        expect(res._getJSONData()).toStrictEqual(newTodo)
-    });
-
-    it('Should handle errors', async function () {
-        const errorMessage = {
-            message: 'Some properties required was not sent'
-        }
-        const rejectedPromise = Promise.reject(errorMessage)
-        TodoModel.create.mockReturnValue(rejectedPromise)
-        await todoController.createTodo(req, res, next)
-        expect(next).toBeCalledWith(errorMessage)
-    });
 })
+
 
 
 
