c1b3779859dc21286e5afbf72ad7ea03b5eaee21
Alan Casas Arevalo <alan.casas.arevalo@everis.com>
2020-10-17 11:30:44 +0200

    [FEATURE][APP] * Todo routes was added with post method to our app

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express()
+const todoRoutes = require('./routes/todo.routes')
 
 app.get('/', (req, res) => {
     res.json('Holllaaaaaaa')
 })
+
+app.use('/', todoRoutes)
 
 app.listen(3000, () => {
     console.log('Server is now running')
