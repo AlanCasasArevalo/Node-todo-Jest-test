1f00247a6b8a95559f1cd7c5bfaa9b5bc4b1d4b2
Alan Casas Arevalo <alan.casas.arevalo@everis.com>
2020-10-17 12:15:29 +0200

    [REFACTOR][APPLICATION] * App was refactor to create just the thing that we need into the app * Server was added to launch the server

--- app.js
+++ app.js
@@ -6,16 +6,12 @@ const mongoDB = require('./mongodb/mongodb.connection')
 mongoDB.connect()
 
 app.use(express.json())
+app.use('/', todoRoutes)
 
 app.get('/', (req, res) => {
     res.json('Holllaaaaaaa')
 })
 
-app.use('/', todoRoutes)
-
-app.listen(3000, () => {
-    console.log('Server is now running')
-})
 
 module.exports = app
 
