1fa5c9e27b770a1c3162325c21d93682338a94da
Alan Casas Arevalo <alan.casas.arevalo@everis.com>
2020-10-17 10:26:28 +0200

    [FEATURE][TODO CONTROLLER] * To fix 201 test we need to add status 201 to our response * To ensure that response is finished we need to add send to our response.

--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -3,6 +3,7 @@ const TodoModel = require("../model/todo.model");
 const createTodo = (req, res, next) => {
     const body = req.body
     TodoModel.create(body)
+    res.status(201).send()
 }
 
 module.exports = {
